cbf38587116695b3734cafea5e1873825f1c4b38
= <tkf.x1os@gmail.com>
2025-08-28 14:00:30 +1200

    more home page

--- src/js/index.js
+++ src/js/index.js
@@ -10,20 +10,18 @@ function text_animate(element,text) {
   let changes = 3;
 	let timeout = 30;
 	charSet = "1234567890#@$&%";
-  for (let letter = 0; letter < virtual_origin.length; letter++) {
+  
+	for (let letter = 0; letter < virtual_origin.length; letter++) {
     let virtual_string_letter = virtual_origin.slice(0, letter);
     for (let randomLetter = 0; randomLetter < changes; randomLetter++) {
       let vslt = virtual_string_letter;
       vslt += charSet.charAt(Math.floor(Math.random() * charSet.length));
       render_list.push(vslt);
     }
-    render_list.push(virtual_origin.slice(0, letter + 1));
+   
+		render_list.push(virtual_origin.slice(0, letter + 1));
   }
 
-  // this tool SOOOOO LONNG but it kinda slaps ngl
-  for (let render = 0; render < render_list.length; render++) {
-    setTimeout(() => {
-        element.innerHTML = `${render_list[render]}${' '.repeat(text.length - render_list[render].length)}`;
-    }, timeout * render);
-  }
+  for (let render = 0; render < render_list.length; render++)
+    setTimeout(() => element.innerHTML = `${render_list[render]}${' '.repeat(text.length - render_list[render].length)}`, timeout * render);
 }
